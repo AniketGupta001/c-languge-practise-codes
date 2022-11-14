97656b4225376b8bae4100fea409c60ee1a85525
aniket <aniketgup7788@gmail.com>
2022-11-14 14:38:38 +0530

    adding prac 13.c or 28.c checking

---
+++ prac13.c
@@ -0,0 +1,16 @@
+//Practise for topic: CALL BY VALUE .[nummber = 4]
+#include<stdio.h>
+void square(int n);
+
+int main(){
+    int number = 4;
+    square(number);
+    printf ("number = %d\n",number);
+    return 0;
+}
+
+//call by value
+void square(int n){
+n = n * n ;
+printf("square = %d\n",n);
+}
