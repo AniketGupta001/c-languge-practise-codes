1a45b04d9a3f4ed25617a75374bff5de6a19ccad
aniket <aniketgup7788@gmail.com>
2022-11-16 21:15:24 +0530

    practiseNo.2

---
+++ prac2.c
@@ -0,0 +1,23 @@
+
+// using a library function to print square of a number by user.
+#include <math.h>
+#include <stdio.h>
+
+int main()
+{
+   double number, squareRoot;
+
+   printf("Enter a number: ");
+   s
+       scanf("%lf", &number);
+
+   // computing the square root
+   squareRoot = sqrt(number);
+   {
+
+      printf("Square root of %.2lf =  %.2lf", number, squareRoot);
+
+      return 0;
+   }
+}
+
